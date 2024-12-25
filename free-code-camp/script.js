404838b4764904487d6992d8a4200b3ed96868fa
SantiagoZ13 <diazsantiagoponce1@gmail.com>
2024-12-25 16:16:36 -0500

    locations text and functions added

--- free-code-camp/script.js
+++ free-code-camp/script.js
@@ -7,38 +7,66 @@ const messageContainer = document.querySelector(".message-container");
 const formContainer = document.querySelector(".form-stats-container");
 const gameContainer = document.querySelector(".game-container");
 
+const btn1 = document.querySelector(".button1");
+const btn2 = document.querySelector(".button2");
+const btn3 = document.querySelector(".button3");
+
+function changeLocation(location) {}
+
 const locations = [
   {
     name: "town square",
     text: "You are in the town square. Where do you want to go?",
+    buttonsTexts: ["Go to store", "Go to cave", "Go to explore"],
+    buttonsFunctions: [GoStore, GoCave, GoExplore],
   },
   {
     name: "cave",
     text: "You are at the cave. You see monsters. What do you want to do?",
+    buttonsTexts: ["Fight the slime", "Fight the fanged beast", "Go to town"],
+    buttonsFunctions: [FightSlime, FightFangedBest, GoTown],
+  },
+  {
+    name: "explore",
+    text: "You are exploring and find this locaions. What do you want to do?",
+    buttonsTexts: ["Go to forest", "Go to magic dungeon", "Go to town"],
+    buttonsFunctions: [GoForest, GoMagicDungeon, GoTown],
   },
   {
     name: "forest",
     text: "You are at the forest. You see a bear. What do you want to do?",
+    buttonsTexts: ["Fight the bear", "Fight the demon", "Go to town"],
+    buttonsFunctions: [FightBear, FightDemon, GoTown],
   },
   {
     name: "magic dungeon",
     text: "You are at the dungeon. You see a dragon. What do you want to do?",
+    buttonsTexts: ["Fight the dragon", "Go to town"],
+    buttonsFunctions: [FightDragon, GoTown],
   },
   {
     name: "fight",
     text: "You are fighting the monster. What do you want to do?",
+    buttonsTexts: ["Attack", "Dodge", "Run"],
+    buttonsFunctions: [Attack, Dodge, Run],
   },
   {
     name: "moster killed",
     text: "You have defeted the moster. You gain experience points and find gold.",
+    buttonsTexts: ["Go to town", "Go to explore"],
+    buttonsFunctions: [GoTown, GoExplore],
   },
   {
     name: "lose",
     text: "You have been defeated by the monster. Game over.",
+    buttonTexts: ["Start over"],
+    buttonsFunctions: [StartOver],
   },
   {
     name: "win",
     text: "You have defeated the dragon. You have wined the game.",
+    buttonTexts: ["Start over"],
+    buttonsFunctions: [StartOver],
   },
 ];
 
@@ -49,7 +77,7 @@ const monsters = [
     health: 15,
   },
   {
-    name: "goblin",
+    name: "bear",
     damage: 4,
     health: 20,
   },
@@ -69,6 +97,7 @@ const monsters = [
     health: 300,
   },
 ];
+
 btnStart.addEventListener("click", () => {
   if (playerName.value === "" || playerClass.value === "") {
     alert("Please fill in all fields!");
