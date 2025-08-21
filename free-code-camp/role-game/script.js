e26cdeedab547721c68dbc21317115cc3e5adb90
SantiagoZ13 <diazsantiagoponce1@gmail.com>
2025-08-21 15:09:00 -0500

    store location added with his respective functions

--- free-code-camp/role-game/script.js
+++ free-code-camp/role-game/script.js
@@ -1,7 +1,16 @@
 let playerName = document.querySelector(".player-name");
 let playerClass = document.querySelector(".player-class");
-const btnStart = document.querySelector(".btn-start");
+let health = 100;
+let gold = 50;
+let xp = 0;
+let inventory = ["stick"];
+let currentWeaponIndex = 0;
 
+const btnStart = document.querySelector(".btn-start");
+const xpText = document.querySelector(".xp-stat");
+const healthText = document.querySelector(".health-stat");
+const goldText = document.querySelector(".gold-stat");
+let weapons = [];
 let messageContainer = document.querySelector(".message-container");
 const formContainer = document.querySelector(".form-stats-container");
 const gameContainer = document.querySelector(".game-container");
@@ -9,6 +18,38 @@ const gameContainer = document.querySelector(".game-container");
 let btn1 = document.querySelector(".button1");
 let btn2 = document.querySelector(".button2");
 let btn3 = document.querySelector(".button3");
+
+btnStart.addEventListener("click", () => {
+  if (playerName.value === "" || playerClass.value === "") {
+    alert("Please fill in all fields!");
+  } else {
+    formContainer.style.display = "none";
+    gameContainer.style.display = "flex";
+    messageContainer.innerHTML = `Welcome, ${playerName.value} the ${playerClass.value}. You must defeat the dragon that is preventing people from leaving the town. You are in the town square. Where do you want to go? Use the buttons above.`;
+    if (playerClass.value === "warrior") {
+      weapons = [
+        { name: "stick", power: 5 },
+        { name: "dagger", power: 30 },
+        { name: "sword", power: 50 },
+        { name: "sacred sword", power: 100 },
+      ];
+    } else if (playerClass.value === "archer") {
+      weapons = [
+        { name: "stick", power: 5 },
+        { name: "bow", power: 30 },
+        { name: "crossbow", power: 50 },
+        { name: "magic bow", power: 100 },
+      ];
+    } else {
+      weapons = [
+        { name: "stick", power: 5 },
+        { name: "basic scepter", power: 30 },
+        { name: "arcane staff", power: 50 },
+        { name: "ancient spellbook", power: 100 },
+      ];
+    }
+  }
+});
 
 btn1.onclick = GoStore;
 btn2.onclick = GoCave;
@@ -20,6 +61,16 @@ const locations = [
     text: "You are in the town square. Where do you want to go?",
     buttonsTexts: ["Go to store", "Go to cave", "Go to explore"],
     buttonsFunctions: [GoStore, GoCave, GoExplore],
+  },
+  {
+    name: "store",
+    text: "You are at store. You can buy new weapons or health potions",
+    buttonsTexts: [
+      "Buy new weapon (30 gold)",
+      "Buy health potion (10 gold)",
+      "Go to town",
+    ],
+    buttonsFunctions: [BuyWeapon, BuyHealth, GoTown],
   },
   {
     name: "cave",
@@ -99,44 +150,53 @@ const monsters = [
   },
 ];
 
-btnStart.addEventListener("click", () => {
-  if (playerName.value === "" || playerClass.value === "") {
-    alert("Please fill in all fields!");
-  } else {
-    formContainer.style.display = "none";
-    gameContainer.style.display = "flex";
-    messageContainer.innerHTML = `Welcome, ${playerName.value} the ${playerClass.value}. You must defeat the dragon that is preventing people from leaving the town. You are in the town square. Where do you want to go? Use the buttons above.`;
-  }
-});
-
 function changeLocation(location) {
+  console.log(weapons);
   locationSelected = locations[location];
-  messageContainer.innerHTML = locationSelected.text;
-  btn1.innerHTML = locationSelected.buttonsTexts[0];
-  btn2.innerHTML = locationSelected.buttonsTexts[1];
-  btn3.innerHTML = locationSelected.buttonsTexts[2];
+  messageContainer.innerText = locationSelected.text;
+  btn1.innerText = locationSelected.buttonsTexts[0];
+  btn2.innerText = locationSelected.buttonsTexts[1];
+  btn3.innerText = locationSelected.buttonsTexts[2];
   btn1.onclick = locationSelected.buttonsFunctions[0];
   btn2.onclick = locationSelected.buttonsFunctions[1];
   btn3.onclick = locationSelected.buttonsFunctions[2];
 }
-function GoStore() {
-  changeLocation(0);
-}
-function GoCave() {
-  changeLocation(1);
-}
-function GoExplore() {
-  changeLocation(2);
-}
 function GoTown() {
   changeLocation(0);
 }
-function GoForest() {
+function GoStore() {
+  changeLocation(1);
+}
+function GoCave() {
+  changeLocation(2);
+}
+function GoExplore() {
   changeLocation(3);
 }
-function GoMagicDungeon() {
+function GoForest() {
   changeLocation(4);
 }
+function GoMagicDungeon() {
+  changeLocation(5);
+}
+
+function BuyWeapon() {
+  if (gold >= 30) {
+    currentWeaponIndex += 1;
+  }
+}
+function BuyHealth() {
+  if (gold >= 10) {
+    gold -= 10;
+    health += 10;
+    goldText.innerText = gold;
+    healthText.innerText = health;
+  } else {
+    messageContainer.innerText =
+      "You don't have enough money to buy more life potions";
+  }
+}
+
 function FightSlime() {}
 function FightFangedBest() {}
 function FightDragon() {}
